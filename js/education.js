e3a4c9ab31efd10fa21753c378df62b62592affb
antonio bh <bh_antonio@outlook.com>
2023-11-01 00:59:45 -0600

    Rename properties in EduHandler class

--- js/education.js
+++ js/education.js
@@ -1,17 +1,17 @@
 
 class EducationHandler
 {
-	#formEdu;
-	#eduName;
-	#eduInstitute;
-	#eduStart;
-	#eduEnd;
-	#eduIndex;
+	#elForm;
+	#elName;
+	#elInstitute;
+	#elStart;
+	#elEnd;
+	#elIndex;
 
 	#$legend;
-	#$btnAuxEdu;
-	#$btnMainEdu;
-	#$tableBodyEdu;
+	#$btnAux;
+	#$btnMain;
+	#$tableBody;
 
 	#enabledMode = true;   // to enable/disable links in the table
 
@@ -45,35 +45,35 @@ class EducationHandler
 	#loadRefs() {
 		console.log('\t\t references');
 
-		this.#formEdu = document.getElementById('formEducation');
-		this.#eduName = this.#formEdu.elements.eduName;
-		this.#eduInstitute = this.#formEdu.elements.eduInstitute;
-	    this.#eduStart = this.#formEdu.elements.eduStart;
-		this.#eduEnd = this.#formEdu.elements.eduEnd;
-		this.#eduIndex = this.#formEdu.elements.eduIndex;
+		this.#elForm = document.getElementById('formEducation');
+		this.#elName = this.#elForm.elements.eduName;
+		this.#elInstitute = this.#elForm.elements.eduInstitute;
+	    this.#elStart = this.#elForm.elements.eduStart;
+		this.#elEnd = this.#elForm.elements.eduEnd;
+		this.#elIndex = this.#elForm.elements.eduIndex;
 
-		this.#$legend = $(this.#formEdu).find('legend');
-		this.#$btnAuxEdu = $(this.#formEdu.elements.btnAuxEdu);
-		this.#$btnMainEdu = $(this.#formEdu.elements.btnMainEdu);
-		this.#$tableBodyEdu = $('table#tableEdu tbody');
+		this.#$legend = $(this.#elForm).find('legend');
+		this.#$btnAux = $(this.#elForm.elements.btnAuxEdu);
+		this.#$btnMain = $(this.#elForm.elements.btnMainEdu);
+		this.#$tableBody = $('table#tableEdu tbody');
 	}
 
 	#initCharCounters() {
 		console.log('\t\t character counters');
-		FormUtil.initCharCounters(this.#formEdu);
+		FormUtil.initCharCounters(this.#elForm);
 	}
 
     #initAuxBtn() {
     	console.log('\t\t aux button');
 
 		let _this = this;
-    	_this.#$btnAuxEdu.on('click', function(e) {   // on click cancel update or just clean form (in insert mode)
+    	_this.#$btnAux.on('click', function(e) {   // on click cancel update or just clean form (in insert mode)
         	e.preventDefault();
 			if (this.textContent === EducationHandler.#BTN_AUX_CANCEL) {
 				_this.#formInInsertMode();   // if an update is canceled, set the form in insert mode and enable links in the table
 				_this.#enableOptions();
 			}
-			FormUtil.reset(_this.#formEdu);
+			FormUtil.reset(_this.#elForm);
 			this.blur();
     	});
     }
@@ -82,7 +82,7 @@ class EducationHandler
     	console.log('\t\t main button');
 
 		let _this = this;
-    	_this.#$btnMainEdu.on('click', function(e) {   // on click update entry or insert a new one
+    	_this.#$btnMain.on('click', function(e) {   // on click update entry or insert a new one
 	        e.preventDefault();
 			if (this.textContent === EducationHandler.#BTN_MAIN_UPDATE) {
 				_this.#update();
@@ -101,19 +101,19 @@ class EducationHandler
 		if ( this.#isValidForm() ) {
 	        let newRow = 
 	        `<tr>
-	            <td>${this.#eduName.value}</td>
-	            <td>${this.#eduInstitute.value}</td>
-				<td>${this.#eduStart.value}</td>
-				<td>${this.#eduEnd.value}</td>
+	            <td>${this.#elName.value}</td>
+	            <td>${this.#elInstitute.value}</td>
+				<td>${this.#elStart.value}</td>
+				<td>${this.#elEnd.value}</td>
 				<td><a href="#" onclick="educationHandler.moveUp(event)">&bigtriangleup;</a></td>
 				<td><a href="#" onclick="educationHandler.moveDown(event)">&bigtriangledown;</a></td>
 	            <td><a href="#" onclick="educationHandler.select(event)">Editar</a></td>
 	            <td><a href="#" onclick="educationHandler.remove(event)">Eliminar</a></td>
 	        </tr>`;
-	        this.#$tableBodyEdu.append(newRow);
-			FormUtil.reset(this.#formEdu);   // clean form only if everything was ok 
+	        this.#$tableBody.append(newRow);
+			FormUtil.reset(this.#elForm);   // clean form only if everything was ok 
 
-			let index = this.#$tableBodyEdu.children().length - 1;
+			let index = this.#$tableBody.children().length - 1;
 			console.log(`[education] row ${index} inserted!`);
 	    }
 	}
@@ -125,13 +125,13 @@ class EducationHandler
 			let index = TableUtil.indexOfRow(event);
 
 			this.#formInUpdateMode();
-			let $row = this.#$tableBodyEdu.children().eq(index).children();
+			let $row = this.#$tableBody.children().eq(index).children();
 
-			this.#eduName.value = $row.eq(EducationHandler.#COL_NAME).text(); this.#eduName.focus();
-			this.#eduInstitute.value = $row.eq(EducationHandler.#COL_INSTITUTE).text(); this.#eduInstitute.focus();
-			this.#eduStart.value = $row.eq(EducationHandler.#COL_START).text(); this.#eduStart.focus();
-			this.#eduEnd.value = $row.eq(EducationHandler.#COL_END).text(); this.#eduEnd.focus();
-			this.#eduIndex.value = index;
+			this.#elName.value = $row.eq(EducationHandler.#COL_NAME).text(); this.#elName.focus();
+			this.#elInstitute.value = $row.eq(EducationHandler.#COL_INSTITUTE).text(); this.#elInstitute.focus();
+			this.#elStart.value = $row.eq(EducationHandler.#COL_START).text(); this.#elStart.focus();
+			this.#elEnd.value = $row.eq(EducationHandler.#COL_END).text(); this.#elEnd.focus();
+			this.#elIndex.value = index;
 
 			this.#disableOptions();   // disable links in the table, while updating an entry
 			console.log(`[education] row ${index} selected!`);
@@ -139,23 +139,23 @@ class EducationHandler
 	}
 
 	#update() {
-		let index = this.#eduIndex.value;
+		let index = this.#elIndex.value;
 
 		if ( this.#isValidForm() ) {
 			let updatedRow = 
-			`<td>${this.#eduName.value}</td>
-	         <td>${this.#eduInstitute.value}</td>
-			 <td>${this.#eduStart.value}</td>
-			 <td>${this.#eduEnd.value}</td>
+			`<td>${this.#elName.value}</td>
+	         <td>${this.#elInstitute.value}</td>
+			 <td>${this.#elStart.value}</td>
+			 <td>${this.#elEnd.value}</td>
 			 <td><a href="#" onclick="educationHandler.moveUp(event)">&bigtriangleup;</a></td>
 			 <td><a href="#" onclick="educationHandler.moveDown(event)">&bigtriangledown;</a></td>
 	         <td><a href="#" onclick="educationHandler.select(event)">Editar</a></td>
 	         <td><a href="#" onclick="educationHandler.remove(event)">Eliminar</a></td>`;
 			 
-			this.#$tableBodyEdu.children().eq(index).html(updatedRow);
+			this.#$tableBody.children().eq(index).html(updatedRow);
 
 			this.#formInInsertMode();  // after an update, return form to insert mode, clean it and enable links in the table
-			FormUtil.reset(this.#formEdu);
+			FormUtil.reset(this.#elForm);
 			this.#enableOptions();
 
 			console.log(`[education] row ${index} updated!`);
@@ -168,7 +168,7 @@ class EducationHandler
 			event.preventDefault();
 			let index = TableUtil.indexOfRow(event);
 
-			let $row = this.#$tableBodyEdu.children().eq(index);
+			let $row = this.#$tableBody.children().eq(index);
 			let eduName = $row.children().eq(EducationHandler.#COL_NAME).text();
 
 			if (confirm(`Confirma eliminaci\xF3n de "${eduName}"`)) {
@@ -182,9 +182,9 @@ class EducationHandler
 	/* form validation */
 
 	#isValidForm() {
-		return Validator.isInputNotEmpty(this.#eduName) * 
-			   Validator.isInputNotEmpty(this.#eduInstitute) * 
-			   Validator.isYearRangeValid(this.#eduStart, this.#eduEnd);
+		return Validator.isInputNotEmpty(this.#elName) * 
+			   Validator.isInputNotEmpty(this.#elInstitute) * 
+			   Validator.isYearRangeValid(this.#elStart, this.#elEnd);
 	}
 
 
@@ -192,32 +192,32 @@ class EducationHandler
 
 	#formInInsertMode() {
 		this.#$legend.html(EducationHandler.#FORM_LEGEND_INSERT);
-		this.#$btnAuxEdu.html(EducationHandler.#BTN_AUX_CLEAN);
-		this.#$btnMainEdu.html(EducationHandler.#BTN_MAIN_INSERT);
+		this.#$btnAux.html(EducationHandler.#BTN_AUX_CLEAN);
+		this.#$btnMain.html(EducationHandler.#BTN_MAIN_INSERT);
 	}
 
 	#formInUpdateMode() {
 		this.#$legend.html(EducationHandler.#FORM_LEGEND_UPDATE);
-		this.#$btnAuxEdu.html(EducationHandler.#BTN_AUX_CANCEL);
-		this.#$btnMainEdu.html(EducationHandler.#BTN_MAIN_UPDATE);
+		this.#$btnAux.html(EducationHandler.#BTN_AUX_CANCEL);
+		this.#$btnMain.html(EducationHandler.#BTN_MAIN_UPDATE);
 	}
 
 
 	/* handle enabled and disabled modes of links */
 
 	#disableOptions() {
-		TableUtil.disableLinks( this.#$tableBodyEdu );
+		TableUtil.disableLinks(this.#$tableBody);
 		this.#enabledMode = false;
 	}
 
 	#enableOptions() {
-		TableUtil.enableLinks( this.#$tableBodyEdu );
+		TableUtil.enableLinks(this.#$tableBody);
 		this.#enabledMode = true;
 	}
 
 	exitDisabledMode() {  // not private because it's called from an external method that handles custom events on the tabs component
 		if (this.#enabledMode == false) {
-			this.#$btnAuxEdu.trigger('click');  // trigger cancel button while updating an entry, if the user moves to another tab
+			this.#$btnAux.trigger('click');  // trigger cancel button while updating an entry, if the user moves to another tab
 		}
 	}
 
@@ -226,13 +226,13 @@ class EducationHandler
 
 	moveUp(event) {
 		if (this.#enabledMode) 
-			TableUtil.moveRowUp(this.#$tableBodyEdu, event);
+			TableUtil.moveRowUp(this.#$tableBody, event);
 	}
 
 	moveDown(event) {
 		// console.log(JSON.stringify(this.getEducation(), null, 2));
 		if (this.#enabledMode)
-			TableUtil.moveRowDown(this.#$tableBodyEdu, event);
+			TableUtil.moveRowDown(this.#$tableBody, event);
 	}
 
 
@@ -241,7 +241,7 @@ class EducationHandler
 	getEducation() {
 		let eduList = new Array();
 
-		this.#$tableBodyEdu.children().each( function(index) {
+		this.#$tableBody.children().each( function(index) {
 			let $tds = $(this).children();
 
 			let nam = $tds.eq(EducationHandler.#COL_NAME).text();
