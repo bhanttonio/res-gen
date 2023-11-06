dce7079cc576a8a318a8106fb555850aff410735
antonio bh <bh_antonio@outlook.com>
2023-11-05 19:50:10 -0600

    Refactor IweTaskHandler

---
+++ js/int-work-exp.js
@@ -0,0 +1,85 @@
+
+class IweTaskHandler extends ModuleHandler
+{
+    static #DESC  = 0;
+    static #INDEX = 1;
+
+    constructor() {
+        super();
+    }
+
+    initRefs() {
+        // dom elements
+        this.elForm  = document.getElementById('formIweTask');
+        let elDesc  = this.elForm.elements.descIweTask;
+        this.elIndex = this.elForm.elements.indexIweTask;
+        this.fields = [elDesc];
+        
+        // jquery objects
+        this.$legend    = $('#labelIweTask');
+        this.$btnAux    = $('#linkAuxIweTask');
+        this.$btnMain   = $('#linkMainIweTask');
+        this.$tableBody = $('table#tableIweTask tbody');
+
+        // aux fields
+        this.formLegend  = 'Descripci&oacute;n';        
+        this.rowType     = RowType.REDUCED;
+        this.handlerName = 'iweTaskHandler';
+    }
+
+    isValidForm() {
+        return Validator.isInputNotEmpty( this.fields[IweTaskHandler.#DESC] );
+    }
+
+    getObject(data) {
+        return new Task(
+            data[IweTaskHandler.#DESC], 
+            data[IweTaskHandler.#INDEX] 
+        );
+    }
+
+}// 
+
+
+
+class IweToolHandler extends ModuleHandler
+{
+    static #DESC  = 0;
+    static #INDEX = 1;
+
+    constructor() {
+        super();
+    }
+
+    initRefs() {
+        // dom elements
+        this.elForm  = document.getElementById('formIweTool');
+        let elDesc  = this.elForm.elements.descIweTool;
+        this.elIndex = this.elForm.elements.indexIweTool;
+        this.fields = [elDesc];
+        
+        // jquery objects
+        this.$legend    = $('#labelIweTool');
+        this.$btnAux    = $('#linkAuxIweTool');
+        this.$btnMain   = $('#linkMainIweTool');
+        this.$tableBody = $('table#tableIweTool tbody');
+
+        // aux fields
+        this.formLegend  = 'Descripci&oacute;n';        
+        this.rowType     = RowType.REDUCED;
+        this.handlerName = 'iweToolHandler';
+    }
+
+    isValidForm() {
+        return Validator.isInputNotEmpty( this.fields[IweToolHandler.#DESC] );
+    }
+
+    getObject(data) {
+        return new Tool(
+            data[IweToolHandler.#DESC], 
+            data[IweToolHandler.#INDEX] 
+        );
+    }
+
+}// 
+
