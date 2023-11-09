9038d8f0b94ac77129c0dab709d4a7f2a7d9c5bd
antonio bh <bh_antonio@outlook.com>
2023-11-09 00:36:44 -0600

    Update HANDLER_NAME props in classes of script int-work-exp

--- js/int-work-exp.js
+++ js/int-work-exp.js
@@ -1,4 +1,94 @@
 
+class IweTaskHandler extends ModuleHandler
+{
+    static #DESC  = 0;
+    static #INDEX = 1;
+
+    constructor() {
+        super();
+    }
+
+    initRefs() {
+        // dom elements
+        this.elForm  = document.getElementById('formIweTask');
+        this.elIndex = this.elForm.elements.indexIweTask;
+
+        let elDesc  = this.elForm.elements.descIweTask;
+        this.fields = [elDesc];
+        
+        // jquery objects
+        this.$legend    = $('#labelIweTask');
+        this.$btnAux    = $('#linkAuxIweTask');
+        this.$btnMain   = $('#linkMainIweTask');
+        this.$tableBody = $('table#tableIweTask tbody');
+
+        // aux values
+        this.FORM_LEGEND  = 'Actividad';
+        this.INSERT_LEGEND = ' &lsqb;Nueva&rsqb;';
+        this.ROW_TYPE     = RowType.REDUCED;
+        this.HANDLER_NAME = 'iweHandler.taskHandler';
+    }
+
+    isValidForm() {
+        return Validator.isInputNotEmpty( this.fields[IweTaskHandler.#DESC] );
+    }
+
+    getObject(data) {
+        return new Task(
+            data[IweTaskHandler.#DESC], 
+            data[IweTaskHandler.#INDEX] 
+        );
+    }
+
+}// 
+
+
+
+class IweToolHandler extends ModuleHandler
+{
+    static #DESC  = 0;
+    static #INDEX = 1;
+
+    constructor() {
+        super();
+    }
+
+    initRefs() {
+        // dom elements
+        this.elForm  = document.getElementById('formIweTool');
+        this.elIndex = this.elForm.elements.indexIweTool;
+
+        let elDesc  = this.elForm.elements.descIweTool;
+        this.fields = [elDesc];
+        
+        // jquery objects
+        this.$legend    = $('#labelIweTool');
+        this.$btnAux    = $('#linkAuxIweTool');
+        this.$btnMain   = $('#linkMainIweTool');
+        this.$tableBody = $('table#tableIweTool tbody');
+
+        // aux values
+        this.FORM_LEGEND  = 'Entorno';
+        this.INSERT_LEGEND = ' &lsqb;Nuevo&rsqb;';
+        this.ROW_TYPE     = RowType.REDUCED;
+        this.HANDLER_NAME = 'iweHandler.toolHandler';
+    }
+
+    isValidForm() {
+        return Validator.isInputNotEmpty( this.fields[IweToolHandler.#DESC] );
+    }
+
+    getObject(data) {
+        return new Tool(
+            data[IweToolHandler.#DESC], 
+            data[IweToolHandler.#INDEX] 
+        );
+    }
+
+}// 
+
+
+
 class IweHandler extends MultipleModuleHandler
 {
     static #ACCOUNT = 0;
@@ -49,96 +139,6 @@ class IweHandler extends MultipleModuleHandler
             data[IweHandler.#PROJECT], 
             data[IweHandler.#PERIOD], 
             data[IweHandler.#INDEX]
-        );
-    }
-
-}// 
-
-
-
-class IweTaskHandler extends ModuleHandler
-{
-    static #DESC  = 0;
-    static #INDEX = 1;
-
-    constructor() {
-        super();
-    }
-
-    initRefs() {
-        // dom elements
-        this.elForm  = document.getElementById('formIweTask');
-        this.elIndex = this.elForm.elements.indexIweTask;
-
-        let elDesc  = this.elForm.elements.descIweTask;
-        this.fields = [elDesc];
-        
-        // jquery objects
-        this.$legend    = $('#labelIweTask');
-        this.$btnAux    = $('#linkAuxIweTask');
-        this.$btnMain   = $('#linkMainIweTask');
-        this.$tableBody = $('table#tableIweTask tbody');
-
-        // aux values
-        this.FORM_LEGEND  = 'Actividad';
-        this.INSERT_LEGEND = ' &lsqb;Nueva&rsqb;';
-        this.ROW_TYPE     = RowType.REDUCED;
-        this.HANDLER_NAME = 'iweTaskHandler';
-    }
-
-    isValidForm() {
-        return Validator.isInputNotEmpty( this.fields[IweTaskHandler.#DESC] );
-    }
-
-    getObject(data) {
-        return new Task(
-            data[IweTaskHandler.#DESC], 
-            data[IweTaskHandler.#INDEX] 
-        );
-    }
-
-}// 
-
-
-
-class IweToolHandler extends ModuleHandler
-{
-    static #DESC  = 0;
-    static #INDEX = 1;
-
-    constructor() {
-        super();
-    }
-
-    initRefs() {
-        // dom elements
-        this.elForm  = document.getElementById('formIweTool');
-        this.elIndex = this.elForm.elements.indexIweTool;
-
-        let elDesc  = this.elForm.elements.descIweTool;
-        this.fields = [elDesc];
-        
-        // jquery objects
-        this.$legend    = $('#labelIweTool');
-        this.$btnAux    = $('#linkAuxIweTool');
-        this.$btnMain   = $('#linkMainIweTool');
-        this.$tableBody = $('table#tableIweTool tbody');
-
-        // aux values
-        this.FORM_LEGEND  = 'Entorno';
-        this.INSERT_LEGEND = ' &lsqb;Nuevo&rsqb;';
-        this.ROW_TYPE     = RowType.REDUCED;
-        this.HANDLER_NAME = 'iweToolHandler';
-    }
-
-    isValidForm() {
-        return Validator.isInputNotEmpty( this.fields[IweToolHandler.#DESC] );
-    }
-
-    getObject(data) {
-        return new Tool(
-            data[IweToolHandler.#DESC], 
-            data[IweToolHandler.#INDEX] 
         );
     }
 
