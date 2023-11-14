a3885c41584f7b06632105dfa05df9c92bdb7a79
antonio bh <bh_antonio@outlook.com>
2023-11-13 21:19:53 -0600

    Add new IweTaskHandler and IweToolHandler classes

--- js/handlers/simple.js
+++ js/handlers/simple.js
@@ -15,7 +15,7 @@ class EduHandler extends Handler
             }, {
                 $tableBody: $('table#tableEdu tbody'),
                 handler: 'eduHandler', 
-                object: new EducationB()
+                object: new Education()
             },
             tableData
         ); 
@@ -25,6 +25,52 @@ class EduHandler extends Handler
         return Validator.isInputNotEmpty(this.form.fields[EduHandler.NAME]) * 
                Validator.isInputNotEmpty(this.form.fields[EduHandler.INSTITUTE]) * 
                Validator.isYearRangeValid(this.form.fields[EduHandler.START], this.form.fields[EduHandler.END]);
+    }
+
+}//
+
+
+class IweTaskHandler extends Handler
+{
+    static DESC = 1; 
+
+    constructor(tableData) {
+        super({
+            elForm:  document.getElementById('formIweTask'), 
+            formLegend: 'Actividad',
+            insertLegend: 'Nueva' 
+        }, {
+            $tableBody: $('table#tableIweTask tbody'),
+            handler: 'iweHandler.taskHandler', 
+            object: new Task()
+        }, 
+        tableData);
+    }
+
+    isValidForm() {
+        return Validator.isInputNotEmpty( this.form.fields[IweTaskHandler.DESC] );
+    }
+
+}//
+
+class IweToolHandler extends Handler
+{
+    static DESC = 1; 
+
+    constructor(tableData) {
+        super({
+            elForm:  document.getElementById('formIweTool'), 
+            formLegend: 'Entorno'
+        }, {
+            $tableBody: $('table#tableIweTool tbody'),
+            handler: 'iweHandler.toolHandler', 
+            object: new Tool()
+        }, 
+        tableData);
+    }
+
+    isValidForm() {
+        return Validator.isInputNotEmpty( this.form.fields[IweToolHandler.DESC] );
     }
 
 }//
