7dd64b6aa98858b5fde945e9796336feec278c7b
antonio bh <bh_antonio@outlook.com>
2023-11-13 21:16:29 -0600

    Add classes Task, Tool and IntWorkExp to the model script

--- js/base/model.js
+++ js/base/model.js
@@ -1,7 +1,18 @@
 
-
-class EducationB {
+class Education {
     name; institute; start; end;
     constructor() { }
+}
+
+class Task {
+    description;
+}
+
+class Tool {
+    description;
+}
+
+class IntWorkExp {
+    account; role; project; period; tasks = []; tools = [];
 }
 
