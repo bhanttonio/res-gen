360f46771aabf0691418a81b35122354facb7eab
antonio bh <bh_antonio@outlook.com>
2023-11-18 23:45:24 -0600

    Delete helper model script

--- js/base/model.js
+++ js/base/model.js
@@ -1,7 +1,24 @@
 
+class Basic {
+	constructor(name, surname1, surname2, level, profile) {
+		this.name = name; 
+		this.surname1 = surname1; 
+		this.surname2 = surname2;
+		this.level = level; 
+		this.profile = profile;
+	}
+}
+
 class Education {
     name; institute; start; end;
-    constructor() { }
+}
+
+class Course {
+    name; location; date;
+}
+
+class Language {
+    name; speak; read; write;
 }
 
 class IntWorkExp {
